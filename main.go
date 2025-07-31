5cb13b8d38b8513d34f71b43d71104da80e9f716
Martijn van der Kleijn <martijn.niji@gmail.com>
2025-08-01 01:24:42 +0200

    feat: add support for Docker, Docker Compose and Docker Swarm

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 /*
-	Copyright (C) 2023  Martijn van der Kleijn
+Copyright (C) 2023 Martijn van der Kleijn
+This file is part of HomeDash sidecar.
 
-	This file is part of HomeDash sidecar.
-
-	This Source Code Form is subject to the terms of the Mozilla Public
-	License, v. 2.0. If a copy of the MPL was not distributed with this
-	file, You can obtain one at http://mozilla.org/MPL/2.0/.
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
 package main
@@ -15,16 +14,17 @@ import (
 	"context"
 	"encoding/json"
 	"io"
+	"log/slog"
 	"net/http"
 	"os"
-	"strconv"
+	"strings"
 	"time"
 
+	"code.vanderkleijn.net/homedash-sidecar/internal/config"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
-	"github.com/google/uuid"
-	log "github.com/sirupsen/logrus"
 )
 
 type ContainerUpdate struct {
@@ -37,52 +37,37 @@ type ContainerInfo struct {
 	Url     string `json:"url"`
 	Icon    string `json:"icon"`
 	Comment string `json:"comment"`
+	Swarm   bool   `json:"swarm_container"`
 }
 
-var homedashServer string
-var homedashInterval string
-var homedashSidecarUUID string
+var logLevel = new(slog.LevelVar)
 
 func main() {
-	// log.SetLevel(log.DebugLevel)
+	// Setup logging
+	logLevel.Set(slog.LevelInfo) // Default to INFO level
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: logLevel,
+	}))
 
-	homedashServer = os.Getenv("HOMEDASH_SERVER")
-	if homedashServer == "" {
-		log.Fatalf("Environment variable HOMEDASH_SERVER not set or empty.")
-	}
+	// Set the default logger for packages that might use slog.Default()
+	slog.SetDefault(logger)
 
-	homedashInterval = os.Getenv("HOMEDASH_INTERVAL")
-	if homedashInterval == "" {
-		homedashInterval = "10"
-		log.Warnf("Environment variable HOMEDASH_INTERVAL not set or empty. Defaulting to once every %v minutes.", homedashInterval)
-	}
+	config.SetLogger(logger)
+	cfg := config.Load()
+	logger.Debug("homedash server url set", "url", cfg.Server)
 
-	interval, err := strconv.Atoi(homedashInterval)
-	if err != nil {
-		log.Fatalf("Failed to parse HOMEDASH_INTERVAL time interval: %s", err)
-	}
-
-	homedashServer = homedashServer + "/api/v1/applications"
-
-	log.Debugf("Will attempt to connect to %s once every %s", homedashServer, homedashInterval)
-
-	// Identify myself
-	homedashSidecarUUID = os.Getenv("HOMEDASH_SIDECAR_UUID")
-	if homedashSidecarUUID == "" {
-		homedashSidecarUUID = uuid.New().String()
-		log.Warnf("Environment variable HOMEDASH_SIDECAR_UUID not set or empty. Generated new UUID for this sidecar. Please set it in your environment for consistency: %s", homedashSidecarUUID)
-	}
+	// Update logging level based on config
+	logLevel.Set(cfg.Loglevel.ToSlogLevel())
 
 	// Create a new Docker client
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
-		log.Fatal(err)
+		logger.Error("unable to connect with Docker API", "error", err)
+		os.Exit(1)
 	}
 
-	// Create the options to filter containers
-	options := types.ContainerListOptions{
+	options := container.ListOptions{
 		Filters: filters.NewArgs(
-			filters.Arg("label", "homedash.name"),
 			filters.Arg("status", "running"),
 		),
 	}
@@ -90,60 +75,111 @@ func main() {
 	// Check for old data and clean up every X minutes
 	go func() {
 		for {
-			applications := retrieveContainers(cli, options)
-
-			log.Debugf("Trying to add: %v", applications)
-
-			err = postContainerInfo(applications)
-			if err != nil {
-				log.Println(err)
-			}
-
-			time.Sleep(time.Duration(interval) * time.Minute)
+			postApps(getApps(cli, options, cfg), cfg)
+			time.Sleep(cfg.Interval)
 		}
 	}()
 
 	select {}
 }
 
-func retrieveContainers(cli *client.Client, options types.ContainerListOptions) []ContainerInfo {
-	applications := make([]ContainerInfo, 0)
+func getApps(cli *client.Client, options container.ListOptions, cfg *config.Config) []ContainerInfo {
+	logger := config.GetLogger()
 
-	// Get a list of containers based on the filter
+	// Get a list of all running containers
 	containers, err := cli.ContainerList(context.Background(), options)
 	if err != nil {
-		log.Fatal(err)
+		logger.Error("unable to determine set of running containers", "error", err)
 	}
 
-	// Print the container information for each active container and post to REST API
+	// Cache for service labels to avoid repeated API calls
+	serviceLabelsCache := make(map[string]map[string]string)
+
+	applications := make([]ContainerInfo, 0)
+
+	// Process each container
 	for _, container := range containers {
-		containerInfo := ContainerInfo{
-			Name:    container.Labels["homedash.name"],
-			Url:     container.Labels["homedash.url"],
-			Icon:    container.Labels["homedash.icon"],
-			Comment: container.Labels["homedash.comment"],
+		isSwarmContainer := false
+
+		// Start with container labels
+		labels := make(map[string]string)
+		for k, v := range container.Labels {
+			if strings.HasPrefix(k, cfg.LabelPrefix) {
+				labels[k] = v
+			}
 		}
-		applications = append(applications, containerInfo)
+
+		// Check if this is a Swarm service container and get service labels
+		if serviceID, exists := container.Labels["com.docker.swarm.service.id"]; exists {
+			isSwarmContainer = true
+
+			// Check cache first
+			if serviceLabels, cached := serviceLabelsCache[serviceID]; cached {
+				logger.Debug("cache hit", "serviceID", serviceID, "labels", serviceLabels)
+
+				// Merge service labels, giving them precedence
+				for k, v := range serviceLabels {
+					if strings.HasPrefix(k, cfg.LabelPrefix) {
+						labels[k] = v
+					}
+				}
+			} else {
+				// Fetch service info from Docker API
+				service, _, err := cli.ServiceInspectWithRaw(context.Background(), serviceID, types.ServiceInspectOptions{})
+				if err != nil {
+					logger.Warn("failed to inspect service", "serviceID", serviceID, "error", err)
+				} else {
+					// Cache the service labels
+					serviceLabelsCache[serviceID] = service.Spec.Labels
+					logger.Debug("caching service labels", "serviceID", serviceID, "labels", service.Spec.Labels)
+
+					// Merge service labels, giving them precedence
+					for k, v := range service.Spec.Labels {
+						if strings.HasPrefix(k, cfg.LabelPrefix) {
+							labels[k] = v
+						}
+					}
+				}
+			}
+		}
+
+		// Only include containers that have at least the homedash.name label
+		if name, exists := labels[cfg.LabelPrefix+"name"]; exists {
+			containerInfo := ContainerInfo{
+				Name:    name,
+				Url:     labels[cfg.LabelPrefix+"url"],
+				Icon:    labels[cfg.LabelPrefix+"icon"],
+				Comment: labels[cfg.LabelPrefix+"comment"],
+				Swarm:   isSwarmContainer,
+			}
+			applications = append(applications, containerInfo)
+			logger.Debug("found application", "container_info", containerInfo)
+		}
 	}
 
 	return applications
 }
 
-func postContainerInfo(applications []ContainerInfo) error {
+func postApps(applications []ContainerInfo, cfg *config.Config) {
+	logger := config.GetLogger()
+
+	logger.Debug("attempting to add apps to server", "apps", applications)
+
 	containerUpdate := ContainerUpdate{
-		Uuid:       homedashSidecarUUID,
+		Uuid:       cfg.UUID,
 		Containers: applications,
 	}
 
 	payload, err := json.Marshal(containerUpdate)
 	if err != nil {
-		return err
+		logger.Error("problem marshalling payload for transmission to server", "error", err)
 	}
 
 	// Create a new HTTP request to the REST API endpoint
-	req, err := http.NewRequest("POST", homedashServer, bytes.NewBuffer(payload))
+	logger.Debug("transmitting payload", "payload", string(payload))
+	req, err := http.NewRequest("POST", cfg.Server, bytes.NewBuffer(payload))
 	if err != nil {
-		return err
+		logger.Error("problem creating HTTP request", "error", err)
 	}
 
 	req.Header.Set("Content-Type", "application/json")
@@ -151,14 +187,11 @@ func postContainerInfo(applications []ContainerInfo) error {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return err
+		logger.Error("problem transmitting payload to server", "error", err)
 	}
-
 	defer resp.Body.Close()
 
 	// Log the response status code for posterity
 	body, _ := io.ReadAll(resp.Body)
-	log.Infof("%d - %s", resp.StatusCode, body)
-
-	return nil
+	logger.Debug("server response", "status_code", resp.StatusCode, "body", string(body))
 }
